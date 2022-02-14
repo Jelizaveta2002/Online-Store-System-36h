88ecd6b9cb573f22c8ea2e40d6a0b33f7eb94675
jevolo <jevolo@ttu.ee>
2022-02-15 00:18:31 +0200

    id codes

--- PR/PR03Ring/src/ee/taltech/iti0202/lotr/Ring.java
+++ PR/PR03Ring/src/ee/taltech/iti0202/lotr/Ring.java
@@ -4,15 +4,15 @@ package ee.taltech.iti0202.lotr;
 
 
 public class Ring {
-    private enum Type {
+    public enum Type {
         THE_ONE, GOLDEN, NENYA, OTHER
     }
 
-    private enum Material {
+    public enum Material {
         GOLD, SILVER, BRONZE, PLASTIC, DIAMOND
     }
-    Type type;
-    Material material;
+    private Type type;
+    private Material material;
     public Ring(Type type, Material material) {
         this.type = type;
         this.material = material;

67a8218ce80adbb520b3b192360b1357a4741a07
jevolo <jevolo@ttu.ee>
2022-02-15 16:41:44 +0200

    id codes

--- PR/PR03Ring/src/ee/taltech/iti0202/lotr/Person.java
+++ PR/PR03Ring/src/ee/taltech/iti0202/lotr/Person.java
@@ -25,20 +25,19 @@ public class Person {
     public String isSauron() {
         if (this.ring != null) {
             if (this.getName().equals("Sauron") && this.ring.getMaterial().equals(Ring.Material.GOLD) &&
-                        this.ring.getType().equals(Ring.Type.THE_ONE)) {
+                        this.ring.getType().equals(Ring.Type.THE_ONE))
                 return "Affirmative";
-                }
             else if (this.getName().equals("Sauron") && !this.ring.getMaterial().equals(Ring.Material.GOLD)
                         && this.ring.getType().equals(Ring.Type.THE_ONE)) {
                 return "No, the ring is fake!";
-                }
+            }
             else if (!this.getName().equals("Sauron") && this.ring.getMaterial().equals(Ring.Material.GOLD)
                         && this.ring.getType().equals(Ring.Type.THE_ONE)) {
                 return "No, he just stole the ring";
-                }
+            }
             else if (this.getName().equals("Sauron") && !this.ring.getType().equals(Ring.Type.THE_ONE)) {
                 return "No, but he's claiming to be";
-                }
+            }
             else {
                 return "No";
             }

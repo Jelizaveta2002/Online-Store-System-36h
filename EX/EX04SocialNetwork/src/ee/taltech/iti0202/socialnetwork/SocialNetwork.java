57fadf552edfc3cbe6e8ed415496ad8ffe88e498
jevolo <jevolo@ttu.ee>
2022-02-20 23:08:29 +0200

    id codes

--- EX/EX04SocialNetwork/src/ee/taltech/iti0202/socialnetwork/SocialNetwork.java
+++ EX/EX04SocialNetwork/src/ee/taltech/iti0202/socialnetwork/SocialNetwork.java
@@ -10,7 +10,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class SocialNetwork {
-    Set<Group> hashOfGroups = new HashSet<>();
+    static Set<Group> hashOfGroups = new HashSet<>();
     public void registerGroup(Group group) {
         if (group != null && ifGroupAlreadyInHash(group)) {
             hashOfGroups.add(group);

f061993425e791eb25029c0c8e725c5a4d020051
jevolo <jevolo@ttu.ee>
2022-02-20 18:48:37 +0200

    id codes

--- EX/EX04SocialNetwork/src/ee/taltech/iti0202/socialnetwork/group/Group.java
+++ EX/EX04SocialNetwork/src/ee/taltech/iti0202/socialnetwork/group/Group.java
@@ -3,40 +3,56 @@ package ee.taltech.iti0202.socialnetwork.group;
 import ee.taltech.iti0202.socialnetwork.message.Message;
 import ee.taltech.iti0202.socialnetwork.user.User;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Group {
+    String name;
+    User owner;
+    ArrayList<Message> listOfMessages = new ArrayList<>();
+    int numOfMessage = -1;
 
+    static Set<User> hashOfUsers = new HashSet<>();
     public Group(String name, User owner) {
-
+        this.name = name;
+        this.owner = owner;
+        if (owner != null) {
+            hashOfUsers.add(owner);
+        }
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public void setName(String name) {
-
+        this.name = name;
     }
 
     public User getOwner() {
-        return null;
+        return owner;
     }
 
     public void addUser(User user) {
-
+        if (user != null) {
+            hashOfUsers.add(user);
+        }
     }
 
     public Set<User> getParticipants() {
-        return null;
+        return hashOfUsers;
     }
 
     public void publishMessage(Message message) {
-
+        if (hashOfUsers.contains(message.getAuthor())) {
+            numOfMessage += 1;
+            listOfMessages.add(numOfMessage, message);
+        }
     }
 
     public List<Message> getMessages() {
-        return null;
+        return listOfMessages;
     }
 }

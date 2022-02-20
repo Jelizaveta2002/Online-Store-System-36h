030ba5ebfeae64223b7430a745dc5deadbab6512
jevolo <jevolo@ttu.ee>
2022-02-20 17:56:24 +0200

    id codes

--- EX/EX04SocialNetwork/src/ee/taltech/iti0202/socialnetwork/feed/Feed.java
+++ EX/EX04SocialNetwork/src/ee/taltech/iti0202/socialnetwork/feed/Feed.java
@@ -1,4 +1,20 @@
 package ee.taltech.iti0202.socialnetwork.feed;
 
+import ee.taltech.iti0202.socialnetwork.message.Message;
+import ee.taltech.iti0202.socialnetwork.user.User;
+
+import java.util.Set;
+
 public class Feed {
+    public Feed(User user, Set<Message> messages) {
+
+    }
+
+    public User getUser() {
+        return null;
+    }
+
+    public Set<Message> getMessages() {
+        return null;
+    }
 }
--- EX/EX04SocialNetwork/src/ee/taltech/iti0202/socialnetwork/message/Message.java
+++ EX/EX04SocialNetwork/src/ee/taltech/iti0202/socialnetwork/message/Message.java
@@ -1,4 +1,21 @@
 package ee.taltech.iti0202.socialnetwork.message;
 
+import ee.taltech.iti0202.socialnetwork.user.User;
+
 public class Message {
+    public Message(String title, String message, User author) {
+
+    }
+
+    public String getTitle() {
+        return null;
+    }
+
+    public String getMessage() {
+        return null;
+    }
+
+    public User getAuthor(){
+        return null;
+    }
 }

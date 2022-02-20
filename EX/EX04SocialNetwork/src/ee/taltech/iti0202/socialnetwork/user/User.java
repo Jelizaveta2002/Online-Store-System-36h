b958f395ed5a98e89af157317cbcac58bef6da37
jevolo <jevolo@ttu.ee>
2022-02-20 17:28:21 +0200

    id codes

---
+++ EX/EX04SocialNetwork/src/ee/taltech/iti0202/socialnetwork/user/User.java
@@ -0,0 +1,19 @@
+package ee.taltech.iti0202.socialnetwork.user;
+
+public class User {
+    public User(String name) {
+
+    }
+
+    public User(String name, Integer age) {
+
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public Integer getAge() {
+        return null;
+    }
+}

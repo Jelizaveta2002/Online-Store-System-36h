5212836baf20b3941750721d08c49254fe1b0f89
jevolo <jevolo@ttu.ee>
2022-02-22 21:26:19 +0200

    id codes

---
+++ EX/EX04SocialNetwork/src/ee/taltech/iti0202/socialnetwork/SocialNetworkTest.java
@@ -0,0 +1,149 @@
+package ee.taltech.iti0202.socialnetwork;
+
+import ee.taltech.iti0202.socialnetwork.feed.Feed;
+import ee.taltech.iti0202.socialnetwork.group.Group;
+import ee.taltech.iti0202.socialnetwork.message.Message;
+import ee.taltech.iti0202.socialnetwork.user.User;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SocialNetworkTest {
+    @Test
+    void userGetters() {
+        User user1 = new User("user1");
+        User user2 = new User("user2", 10);
+        User user3 = new User("user2", 20);
+        String checkName = "user2";
+        assertEquals(checkName, user2.getName());
+    }
+
+    @Test
+    void userGetters2() {
+        User user1 = new User("user1");
+        User user2 = new User("user2", 10);
+        User user3 = new User("user2", 20);
+        Integer checkAge = 10;
+        assertEquals(checkAge, user2.getAge());
+    }
+
+    @Test
+    void userGetters3() {
+        User user1 = new User("user1");
+        User user2 = new User("user2", 10);
+        User user3 = new User("user2", 20);
+        Integer checkAge = null;
+        assertEquals(checkAge, user1.getAge());
+    }
+
+    @Test
+    void messageGetter() {
+        User user1 = new User("user1");
+        Group group2 = new Group("group1", user1);
+        Message message1 = new Message("title1", "content1", user1);
+        String title = "title1";
+        assertEquals(title, message1.getTitle());
+    }
+
+    @Test
+    void messageGetter2() {
+        User user1 = new User("user1");
+        Group group2 = new Group("group1", user1);
+        Message message1 = new Message("title1", "content1", user1);
+        String message = "content1";
+        assertEquals(message, message1.getMessage());
+    }
+
+    @Test
+    void messageGetter3() {
+        User user1 = new User("user1");
+        Group group2 = new Group("group1", user1);
+        Message message1 = new Message("title1", "content1", user1);
+        User user = user1;
+        assertEquals(user, message1.getAuthor());
+    }
+
+    @Test
+    void groupGetter() {
+        User user1 = new User("user1");
+        Group group2 = new Group("group1", user1);
+        Message message1 = new Message("title1", "content1", user1);
+        String name = "group1";
+        assertEquals(name, group2.getName());
+    }
+
+    @Test
+    void groupGetter2() {
+        User user1 = new User("user1");
+        Group group2 = new Group("group1", user1);
+        Message message1 = new Message("title1", "content1", user1);
+        User name = user1;
+        assertEquals(name, group2.getOwner());
+    }
+
+    @Test
+    void getParticipants() {
+        User user1 = new User("user1");
+        User user2 = new User("user2", 10);
+        User user3 = new User("user2", 20);
+        Group group2 = new Group("group1", user1);
+        group2.addUser(user2);
+        group2.addUser(user3);
+        Set<User> set = new HashSet<User>();
+        set.add(user1);
+        set.add(user2);
+        set.add(user3);
+        assertEquals(set, group2.getParticipants());
+    }
+
+    @Test
+    void getMessages() {
+        User user1 = new User("user1");
+        User user2 = new User("user2", 10);
+        User user3 = new User("user2", 20);
+        Group group2 = new Group("group1", user1);
+        group2.addUser(user2);
+        Message message1 = new Message("title1", "content1", user1);
+        Message message2 = new Message("title2", "content2", user2);
+        Message message3 = new Message("title3", "content3", user3);
+        group2.publishMessage(message1);
+        group2.publishMessage(message2);
+        group2.publishMessage(message3);
+        List<Message> checkList = Arrays.asList(message1, message2);
+        assertEquals(checkList, group2.getMessages());
+    }
+
+    @Test
+    void getGroups() {
+        SocialNetwork socialNetwork = new SocialNetwork();
+        User user1 = new User("user1");
+        User user2 = new User("user2", 10);
+        User user3 = new User("user2", 20);
+        Group group2 = new Group("group1", user1);
+        Group group3 = new Group("group2", user2);
+        socialNetwork.registerGroup(group2);
+        socialNetwork.registerGroup(group3);
+        Set<Group> set = new HashSet<Group>();
+        set.add(group2);
+        set.add(group3);
+        assertEquals(set, socialNetwork.getGroups());
+    }
+
+    @Test
+    void getFeedForUser() {
+        SocialNetwork socialNetwork = new SocialNetwork();
+        User user1 = new User("user1", 18);
+        User user2 = new User("user2", 10);
+        Group group = new Group("group1", user1);
+        Message message = new Message("title", "message", user1);
+        socialNetwork.registerGroup(group);
+        group.addUser(user2);
+        group.publishMessage(message);
+        Feed feed1 = socialNetwork.getFeedForUser(user2);
+        Feed feed2 = new Feed(user2, Set.of(message));
+        assertEquals(feed1.getUser(), feed1.getUser());
+        assertEquals(feed2.getMessages(), feed2.getMessages());
+    }
+}

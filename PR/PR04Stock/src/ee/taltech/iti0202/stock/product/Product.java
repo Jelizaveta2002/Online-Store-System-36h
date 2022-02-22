12bb0e5eaa950910f38d885323bb3833d8f8631f
jevolo <jevolo@ttu.ee>
2022-02-22 09:21:37 +0200

    id codes

---
+++ PR/PR04Stock/src/ee/taltech/iti0202/stock/product/Product.java
@@ -0,0 +1,55 @@
+package ee.taltech.iti0202.stock.product;
+import ee.taltech.iti0202.stock.exceptions.StockException;
+
+public class Product {
+
+    /**
+     * Create a new product with the given name and price.
+     * <p>
+     * If the price is negative, throw a StockException with a NEGATIVE_PRICE reason.
+     *
+     * @param name Name of the product.
+     * @param price Price of the product.
+     * @throws StockException NEGATIVE_PRICE
+     */
+    public Product(String name, int price) throws StockException {
+    }
+
+    /**
+     * Returns the next id.
+     *
+     * This value has to increment with every call.
+     *
+     * @return The next id.
+     */
+    public static int getNextId() {
+        return 0;
+    }
+
+    /**
+     * Returns id of the product.
+     *
+     * @return id of the product.
+     */
+    public int getId() {
+        return 0;
+    }
+
+    /**
+     * Returns the name of the product.
+     *
+     * @return the name of the product.
+     */
+    public String getName() {
+        return null;
+    }
+
+    /**
+     * Returns the price of the product.
+     *
+     * @return the price of the product.
+     */
+    public int getPrice() {
+        return 0;
+    }
+}

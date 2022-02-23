d9b302e8bc8c00d9a8cd48ebfd54a7e5674bff62
jevolo <jevolo@ttu.ee>
2022-02-23 14:08:31 +0200

    id codes

---
+++ TK/TK1/src/ee/taltech/iti0202/tk1/art/Collector.java
@@ -0,0 +1,17 @@
+package ee.taltech.iti0202.tk1.art;
+
+import java.util.List;
+
+public class Collector {
+    public boolean addPainting(Painting painting) {
+        return false;
+    }
+
+    public boolean sellPainting(Painting painting, Collector fellowCollector) {
+        return false;
+    }
+
+    public List<Painting> getPaintings() {
+        return null;
+    }
+}
---
+++ TK/TK1/src/ee/taltech/iti0202/tk1/art/Painting.java
@@ -0,0 +1,18 @@
+package ee.taltech.iti0202.tk1.art;
+
+public class Painting {
+
+    public String getTitle() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Painting{}";
+    }
+
+    public String getAuthor() {
+        return null;
+    }
+
+}

ab16194fc808144722395998fbb393f72a03fb8e
jevolo <jevolo@ttu.ee>
2022-02-23 14:12:02 +0200

    id codes

--- TK/TK1/src/ee/taltech/iti0202/tk1/art/Painting.java
+++ TK/TK1/src/ee/taltech/iti0202/tk1/art/Painting.java
@@ -1,6 +1,12 @@
 package ee.taltech.iti0202.tk1.art;
 
 public class Painting {
+    private String author;
+    private String title;
+    public Painting(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
 
     public String getTitle() {
         return null;

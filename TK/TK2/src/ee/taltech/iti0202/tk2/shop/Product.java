5ecc611b221d4283ff9e85748ab5e997f69f445f
jevolo <jevolo@ttu.ee>
2022-02-25 19:06:25 +0200

    id codes

---
+++ TK/TK2/src/ee/taltech/iti0202/tk2/shop/Product.java
@@ -0,0 +1,27 @@
+package ee.taltech.iti0202.tk2.shop;
+
+public class Product {
+    private String name;
+    private Integer price;
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product(String name) {
+        this.name = name;
+        this.price = null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public String toString() {
+        return name + " " + "(" + price.toString() + ")";
+    }
+}
---
+++ TK/TK2/src/ee/taltech/iti0202/tk2/shop/Shop.java
@@ -0,0 +1,19 @@
+package ee.taltech.iti0202.tk2.shop;
+
+import java.util.List;
+import java.util.Optional;
+
+public class Shop {
+
+    boolean addProduct(Product product) {
+        return false;
+    }
+
+    Optional<Product> sellProduct(String name, int maxPrice) {
+        return Optional.empty();
+    }
+
+    List<Product> getProducts() {
+        return null;
+    }
+}

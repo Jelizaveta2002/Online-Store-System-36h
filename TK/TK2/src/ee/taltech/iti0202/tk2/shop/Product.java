ff5f1546097b3c453c2f963b859eddf823be6ddb
jevolo <jevolo@ttu.ee>
2022-02-25 19:26:24 +0200

    id codes

--- TK/TK2/src/ee/taltech/iti0202/tk2/shop/Product.java
+++ TK/TK2/src/ee/taltech/iti0202/tk2/shop/Product.java
@@ -1,25 +1,75 @@
 package ee.taltech.iti0202.tk2.shop;
 
 public class Product {
-    private String name;
+    private final String name;
     private int price;
+
+    /**
+
+     Given a string and an int n, return a string made of n repetitions of the last n characters
+     of the string. You may assume that n is between 0 and the length of the string, inclusive.
+
+     repeatEnd("Hello", 3) → "llollollo"
+     repeatEnd("Hello", 2) → "lolo"
+     repeatEnd("Hello", 1) → "o"
+     */
     public Product(String name, int price) {
         this.name = name;
         this.price = price;
     }
 
+
+    /**
+
+     Given a string and an int n, return a string made of n repetitions of the last n characters
+     of the string. You may assume that n is between 0 and the length of the string, inclusive.
+
+     repeatEnd("Hello", 3) → "llollollo"
+     repeatEnd("Hello", 2) → "lolo"
+     repeatEnd("Hello", 1) → "o"
+     */
     public Product(String name) {
         this.name = name;
     }
 
+
+    /**
+
+     Given a string and an int n, return a string made of n repetitions of the last n characters
+     of the string. You may assume that n is between 0 and the length of the string, inclusive.
+
+     repeatEnd("Hello", 3) → "llollollo"
+     repeatEnd("Hello", 2) → "lolo"
+     repeatEnd("Hello", 1) → "o"
+     */
     public String getName() {
         return name;
     }
 
+
+    /**
+
+     Given a string and an int n, return a string made of n repetitions of the last n characters
+     of the string. You may assume that n is between 0 and the length of the string, inclusive.
+
+     repeatEnd("Hello", 3) → "llollollo"
+     repeatEnd("Hello", 2) → "lolo"
+     repeatEnd("Hello", 1) → "o"
+     */
     public Integer getPrice() {
         return price;
     }
 
+
+    /**
+
+     Given a string and an int n, return a string made of n repetitions of the last n characters
+     of the string. You may assume that n is between 0 and the length of the string, inclusive.
+
+     repeatEnd("Hello", 3) → "llollollo"
+     repeatEnd("Hello", 2) → "lolo"
+     repeatEnd("Hello", 1) → "o"
+     */
     public String toString() {
         return null;
     }
--- TK/TK2/src/ee/taltech/iti0202/tk2/shop/Shop.java
+++ TK/TK2/src/ee/taltech/iti0202/tk2/shop/Shop.java
@@ -9,10 +9,30 @@ public class Shop {
         return false;
     }
 
+
+    /**
+
+     Given a string and an int n, return a string made of n repetitions of the last n characters
+     of the string. You may assume that n is between 0 and the length of the string, inclusive.
+
+     repeatEnd("Hello", 3) → "llollollo"
+     repeatEnd("Hello", 2) → "lolo"
+     repeatEnd("Hello", 1) → "o"
+     */
     public Optional<Product> sellProduct(String name, int maxPrice) {
         return Optional.empty();
     }
 
+
+    /**
+
+     Given a string and an int n, return a string made of n repetitions of the last n characters
+     of the string. You may assume that n is between 0 and the length of the string, inclusive.
+
+     repeatEnd("Hello", 3) → "llollollo"
+     repeatEnd("Hello", 2) → "lolo"
+     repeatEnd("Hello", 1) → "o"
+     */
     public List<Product> getProducts() {
         return null;
     }

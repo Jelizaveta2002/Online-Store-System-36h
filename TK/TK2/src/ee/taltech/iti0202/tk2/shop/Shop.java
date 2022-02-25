5eadf6016cf58146c0f0101faf1262f1711d0db1
jevolo <jevolo@ttu.ee>
2022-02-25 19:27:40 +0200

    id codes

--- TK/TK2/src/ee/taltech/iti0202/tk2/shop/Shop.java
+++ TK/TK2/src/ee/taltech/iti0202/tk2/shop/Shop.java
@@ -5,6 +5,16 @@ import java.util.Optional;
 
 public class Shop {
 
+
+    /**
+
+     Given a string and an int n, return a string made of n repetitions of the last n characters
+     of the string. You may assume that n is between 0 and the length of the string, inclusive.
+
+     repeatEnd("Hello", 3) → "llollollo"
+     repeatEnd("Hello", 2) → "lolo"
+     repeatEnd("Hello", 1) → "o"
+     */
     public boolean addProduct(Product product) {
         return false;
     }

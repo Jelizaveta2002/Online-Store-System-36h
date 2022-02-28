466b09050a82f20dfba33e1fa8e8ec13c7206d1f
jevolo <jevolo@ttu.ee>
2022-02-28 18:03:23 +0200

    id codes

--- EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/factory/OrbFactory.java
+++ EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/factory/OrbFactory.java
@@ -4,20 +4,32 @@ import ee.taltech.iti0202.mysticorbs.orb.Orb;
 import ee.taltech.iti0202.mysticorbs.oven.Oven;
 import ee.taltech.iti0202.mysticorbs.storage.ResourceStorage;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OrbFactory {
     private final ResourceStorage resourceStorage;
+    private final ArrayList<Oven> listOfOvens = new ArrayList<>();
     public OrbFactory(ResourceStorage resourceStorage) {
         this.resourceStorage = resourceStorage;
     }
 
     public void addOven(Oven oven) {
-
+        if (!listOfOvens.isEmpty() && oven.getName() != null && oven.getResourceStorage() != null) {
+            for (Oven oneOven : listOfOvens) {
+                if (oven.getResourceStorage() == oneOven.getResourceStorage() && oven.getName().equals(oneOven.getName())) {
+                    return;
+                }
+            }
+            listOfOvens.add(oven);
+        }
+        if (oven.getName() != null && oven.getResourceStorage() != null) {
+            listOfOvens.add(oven);
+        }
     }
 
     public List<Oven> getOvens() {
-        return null;
+        return listOfOvens;
     }
 
     public List<Orb> getAndClearProducedOrbsList() {

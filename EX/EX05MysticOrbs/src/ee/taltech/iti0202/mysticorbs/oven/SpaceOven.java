4c90c1a98b63f7e631e9a137d108319f7c0f5db5
jevolo <jevolo@ttu.ee>
2022-02-28 17:34:28 +0200

    id codes

--- EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/oven/SpaceOven.java
+++ EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/oven/SpaceOven.java
@@ -2,39 +2,73 @@ package ee.taltech.iti0202.mysticorbs.oven;
 
 import ee.taltech.iti0202.mysticorbs.orb.MagicOrb;
 import ee.taltech.iti0202.mysticorbs.orb.Orb;
+import ee.taltech.iti0202.mysticorbs.orb.SpaceOrb;
 import ee.taltech.iti0202.mysticorbs.storage.ResourceStorage;
 
 import java.util.Optional;
 
 public class SpaceOven extends Oven {
     private int amountOfOrbs = 0;
+    private boolean Broken = false;
     public SpaceOven(String name, ResourceStorage resourceStorage) {
         super(name, resourceStorage);
     }
 
-    public Optional<Orb> craftOrb() {
-        if (!super.getResourceStorage().isEmpty() && helpMethod("gold") && helpMethod("dust")) {
-            super.getResourceStorage().takeResource("gold", 1);
-            super.getResourceStorage().takeResource("dust", 3);
-            amountOfOrbs += 1;
-            if (amountOfOrbs == 2 || amountOfOrbs == 4) {
-                Orb value = new MagicOrb(this.getName());
-                value.charge("gold", 1);
-                value.charge("dust", 3);
-                return Optional.of(value);
+    public boolean helpMethodSpaceOrb(String resource) {
+        if (!super.getResourceStorage().returnMap().isEmpty()) {
+            for (String existedResource : super.getResourceStorage().returnMap().keySet()) {
+                if (resource.equalsIgnoreCase(existedResource) && super.getResourceStorage().returnMap().get(existedResource) >= 15 && resource.equalsIgnoreCase("star fragment")) {
+                    return true;
+                }
+                if (resource.equalsIgnoreCase(existedResource) && super.getResourceStorage().returnMap().get(existedResource) >= 1 && resource.equalsIgnoreCase("meteorite stone")){
+                    return true;
+                }
             }
+            return false;
+        }
+        return false;
+    }
+
+    public boolean helpMethodStandardOrb(String resource) {
+        if (!super.getResourceStorage().returnMap().isEmpty()) {
+            for (String existedResource : super.getResourceStorage().returnMap().keySet()) {
+                if (resource.equalsIgnoreCase(existedResource) && super.getResourceStorage().returnMap().get(existedResource) >= 1 && resource.equalsIgnoreCase("pearl")) {
+                    return true;
+                }
+                if (resource.equalsIgnoreCase(existedResource) && super.getResourceStorage().returnMap().get(existedResource) >= 1 && resource.equalsIgnoreCase("silver")){
+                    return true;
+                }
+            }
+            return false;
+        }
+        return false;
+    }
+
+    public Optional<Orb> craftOrb() {
+        if (!Broken && !super.getResourceStorage().isEmpty() && helpMethodSpaceOrb("meteorite stone") && helpMethodSpaceOrb("star fragment")) {
+            super.getResourceStorage().takeResource("star fragment", 15);
+            super.getResourceStorage().takeResource("meteorite stone", 1);
+            amountOfOrbs += 1;
+            if (amountOfOrbs == 25) {
+                Broken = true;
+            }
+            Orb value = new SpaceOrb(this.getName());
+            return Optional.of(value);
+        }
+        if (Broken && !super.getResourceStorage().isEmpty() && helpMethodStandardOrb("pearl") && helpMethodStandardOrb("silver")) {
+            super.getResourceStorage().takeResource("star fragment", 15);
+            super.getResourceStorage().takeResource("meteorite stone", 1);
             Orb value = new Orb(this.getName());
-            value.charge("gold", 1);
-            value.charge("dust", 3);
+            amountOfOrbs += 1;
             return Optional.of(value);
         }
         return Optional.empty();
     }
 
     public boolean isBroken() {
-        return false;
+        return this.Broken;
     }
     public int getCreatedOrbsAmount() {
-        return 0;
+        return amountOfOrbs;
     }
 }

fd4209cbaffe6aade0f5e3a984100cb6fb105711
jevolo <jevolo@ttu.ee>
2022-02-28 16:14:16 +0200

    id codes

--- EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/orb/SpaceOrb.java
+++ EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/orb/SpaceOrb.java
@@ -3,7 +3,7 @@ package ee.taltech.iti0202.mysticorbs.orb;
 import java.util.HashMap;
 
 public class SpaceOrb extends Orb {
-    private Integer energy = 100;
+    private Integer energy = 0;
     private boolean absorbed = false;
     private boolean isSpaceOrb = true;
     private HashMap<String, Integer> mapOfOrbs = new HashMap<String, Integer>();
@@ -13,7 +13,7 @@ public class SpaceOrb extends Orb {
     }
 
     public boolean returnIsSpace() {
-        return this.isSpaceOrb;
+        return isSpaceOrb;
     }
 
     public void charge(String resource, int amount) {}
@@ -31,11 +31,19 @@ public class SpaceOrb extends Orb {
     }
 
     public boolean absorb(Orb orb) {
-        if (!orb.returnState() && orb.getEnergy() < this.getEnergy() && !orb.returnIsSpace()) {
+        if (!orb.returnState() && orb.getEnergy() < this.getEnergy()) {
             this.energy += orb.getEnergy();
             orb.changeState();
             return true;
         }
         return false;
+    }
+
+    public static void main(String[] args) {
+        Orb orbToAbsorb = new SpaceOrb("OtherCreator");
+        orbToAbsorb.charge("candies", 100);
+        SpaceOrb spaceOrb = new SpaceOrb("Creator");
+        spaceOrb.absorb(orbToAbsorb);
+        System.out.println(spaceOrb.getEnergy());
     }
 }

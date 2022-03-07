d4edf8b8142f670952c7d2c1f42b04b9b41b2b83
jevolo <jevolo@ttu.ee>
2022-03-07 23:15:33 +0200

    id codes

--- EX/EX06Files/src/ee/taltech/iti0202/files/input/InputFilesLines.java
+++ EX/EX06Files/src/ee/taltech/iti0202/files/input/InputFilesLines.java
@@ -1,11 +1,37 @@
 package ee.taltech.iti0202.files.input;
 
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class InputFilesLines implements InputFilesReader {
 
     @Override
     public List<String> readTextFromFile(String filename) {
-        return null;
+        List<String> newList = new ArrayList<>();
+        File file=new File(filename);    //creates a new file instance
+        try(Stream<String> stream = Files.lines(Paths.get(file.toString())))
+        {
+            FileReader fr=new FileReader(file);   //reads the file
+            Scanner sc=new Scanner(fr);  //creates a buffering character input stream
+            String line;
+            while((sc.hasNextLine()))
+            {
+                newList.add(sc.nextLine());      //appends line to string buffer
+            }
+            fr.close();    //closes the stream and release the resources
+        }
+        catch(Exception e)
+        {
+            // e.printStackTrace();
+            throw new FileReaderException(e, "No such file");
+
+        }
+        return newList;
     }
 }

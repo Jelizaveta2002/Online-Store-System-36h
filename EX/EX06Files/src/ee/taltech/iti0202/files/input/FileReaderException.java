554808397cc252d46124061ab06a4b60538247df
jevolo <jevolo@ttu.ee>
2022-03-07 19:44:51 +0200

    id codes

--- EX/EX06Files/src/ee/taltech/iti0202/files/input/FileReaderException.java
+++ EX/EX06Files/src/ee/taltech/iti0202/files/input/FileReaderException.java
@@ -1,4 +1,11 @@
 package ee.taltech.iti0202.files.input;
 
 public class FileReaderException extends RuntimeException {
+    public String getMessage() {
+        return null;
+    }
+
+    public String getCause() {
+        return null;
+    }
 }

bb2d59dd20f3b3b0f49635e24787063bc43079d2
jevolo <jevolo@ttu.ee>
2022-03-08 15:13:38 +0200

    id codes

--- EX/EX06Files/src/ee/taltech/iti0202/files/morse/MorseTranslator.java
+++ EX/EX06Files/src/ee/taltech/iti0202/files/morse/MorseTranslator.java
@@ -1,11 +1,18 @@
 package ee.taltech.iti0202.files.morse;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MorseTranslator {
 
     public Map<String, String> addMorseCodes(List<String> lines) {
-        return null;
+        Map<String, String> mapWithMorse = new HashMap<>();
+        for (String line : lines) {
+            String key = String.valueOf(line.charAt(0));
+            StringBuilder newBuilder = new StringBuilder();
+            newBuilder.append(line);
+            newBuilder.deleteCharAt(0);
+            mapWithMorse.put(key.toLowerCase(Locale.ROOT), newBuilder.toString());
+        }
+        return mapWithMorse;
     }
 
     public List<String> translateLinesToMorse(List<String> lines) {
@@ -22,5 +29,15 @@ public class MorseTranslator {
 
     private String translateLineFromMorse(String line) {
         return null;
+    }
+
+    public static void main(String[] args) {
+        MorseTranslator translator = new MorseTranslator();
+        List<String> newList = new ArrayList<>();
+        newList.add("6--");
+        newList.add("7, --..--");
+        newList.add("0--..--");
+        newList.add("U.--");
+        System.out.println(translator.addMorseCodes(newList));
     }
 }

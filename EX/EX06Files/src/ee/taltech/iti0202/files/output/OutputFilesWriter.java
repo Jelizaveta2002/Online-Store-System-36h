1a1f08f98dece3ff4829722f6c5a514d83783370
jevolo <jevolo@ttu.ee>
2022-03-08 14:18:38 +0200

    id codes

--- EX/EX06Files/src/ee/taltech/iti0202/files/output/OutputFilesWriter.java
+++ EX/EX06Files/src/ee/taltech/iti0202/files/output/OutputFilesWriter.java
@@ -1,10 +1,39 @@
 package ee.taltech.iti0202.files.output;
 
+import ee.taltech.iti0202.files.input.FileReaderException;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class OutputFilesWriter {
 
     public boolean writeLinesToFile(List<String> lines, String filename) {
-        return false;
+        try
+        {
+            FileWriter myWriter = new FileWriter(filename);
+            for (String line : lines) {
+                myWriter.write(line);
+            }
+            myWriter.close();
+            return true;
+        }
+        catch(Exception e)
+        {
+            // e.printStackTrace();
+            throw new FileReaderException(e, "No such file");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        List<String> newList = new ArrayList<>();
+        OutputFilesWriter newWriter = new OutputFilesWriter();
+        System.out.println(newWriter.writeLinesToFile(newList, null));
     }
 }

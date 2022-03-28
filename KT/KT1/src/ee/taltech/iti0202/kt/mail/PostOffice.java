7d6b2af071c5a95d0208fb0530306715152a4539
jevolo <jevolo@ttu.ee>
2022-03-28 17:27:57 +0200

    id codes

--- KT/KT1/src/ee/taltech/iti0202/kt/mail/PostOffice.java
+++ KT/KT1/src/ee/taltech/iti0202/kt/mail/PostOffice.java
@@ -1,11 +1,16 @@
 package ee.taltech.iti0202.kt.mail;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PostOffice {
+    private String location;
+    ArrayList<Letter> lettersInOffice = new ArrayList<>();
+    ArrayList<Postman> postmenInOffice = new ArrayList<>();
     /**
      * Create a post office with the location.
      */
     public PostOffice(String location) {
+        this.location = location;
     }
 
     /**
@@ -13,6 +18,11 @@ public class PostOffice {
      * Letter is added only if the letter's destination city matches the location of the office.
      */
     public void addLetter(Letter letter) {
+        if (letter.getDestinationCity() != null && !letter.getDestinationCity().trim().isEmpty()) {
+            if (this.location.equals(letter.getDestinationCity())) {
+                this.lettersInOffice.add(letter);
+            }
+        }
     }
 
     /**
@@ -22,20 +32,32 @@ public class PostOffice {
      * Otherwise postman is added to the office.
      */
     public void addPostman(Postman postman) {
+        if (postman != null && postman.getName() != null && !postman.getName().trim().isEmpty()) {
+            for (Postman man : this.postmenInOffice) {
+                char firstChar = man.getName().charAt(0);
+                String firstLetter = Character.toString(firstChar);
+                char firstPostC = postman.getName().charAt(0);
+                String firstPost = Character.toString(firstPostC);
+                if (firstLetter.equals(firstPost)) {
+                    return;
+                }
+                postmenInOffice.add(postman);
+            }
+        }
     }
 
     /**
      * Returns all the letter in the post office.
      */
     public List<Letter> getAllLetters() {
-        return null;
+        return this.lettersInOffice;
     }
 
     /**
      * Returns all the postmen in the office.
      */
     public List<Postman> getPostmen() {
-        return null;
+        return this.postmenInOffice;
     }
 
     /**

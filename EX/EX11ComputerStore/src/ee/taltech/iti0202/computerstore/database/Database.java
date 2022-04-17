d87b291f81931cda10f695c8bf1de03ee6d60e89
jevolo <jevolo@ttu.ee>
2022-04-17 14:50:44 +0300

    Delivery

--- EX/EX11ComputerStore/src/ee/taltech/iti0202/computerstore/database/Database.java
+++ EX/EX11ComputerStore/src/ee/taltech/iti0202/computerstore/database/Database.java
@@ -42,7 +42,7 @@ public final class Database {
     }
 
     public Map<Integer, Component> getComponents() {
-        return null;
+        return this.components;
     }
 
     public void resetEntireDatabase() {

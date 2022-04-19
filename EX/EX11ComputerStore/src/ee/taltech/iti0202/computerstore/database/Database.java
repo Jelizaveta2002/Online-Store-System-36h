5f435b5edadd46df20cf19ec9dcf16a0b6458548
jevolo <jevolo@ttu.ee>
2022-04-19 12:03:27 +0300

    Delivery

--- EX/EX11ComputerStore/src/ee/taltech/iti0202/computerstore/database/Database.java
+++ EX/EX11ComputerStore/src/ee/taltech/iti0202/computerstore/database/Database.java
@@ -90,23 +90,21 @@ public final class Database {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         try {
             Files.writeString(Paths.get(location), gson.toJson(this));
-        } catch (IOException e) {
-            System.out.println("IOException:" + e.getMessage());
-            e.printStackTrace();
+        } catch (IOException ignored) {
         }
     }
 
     public void loadFromFile(String location) {
-//        Gson gson = new Gson();
-//        Path path = Paths.get(location);
-//        try (Scanner scanner = new Scanner(path)) {
-//            while (scanner.hasNext()) {
-//                String line = scanner.nextLine();
-//                instance = gson.fromJson(line, Database.class);
-//            }
-//        } catch (IOException e) {
-//            //System.out.println("IOException:" + e.getMessage());
-//            e.printStackTrace();
-//        }
+        Gson gson = new Gson();
+        Path path = Paths.get(location);
+        try (Scanner scanner = new Scanner(path)) {
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                instance = gson.fromJson(line, Database.class);
+            }
+        } catch (IOException e) {
+            //System.out.println("IOException:" + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }

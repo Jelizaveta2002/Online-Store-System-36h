e7c4da3184144ab83663e1afb23ca62fbddeccff
jevolo <jevolo@ttu.ee>
2022-05-02 22:00:24 +0300

    Delivery

--- EXAM/EXAM0/src/ee/taltech/iti0202/exam/timetable/Timetable.java
+++ EXAM/EXAM0/src/ee/taltech/iti0202/exam/timetable/Timetable.java
@@ -1,14 +1,47 @@
 package ee.taltech.iti0202.exam.timetable;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class Timetable {
+    public HashMap<String, Integer> tasks = new HashMap<>();
+    public HashMap<Integer, Integer> days = new HashMap<>();
+    public HashMap<Integer, ArrayList<String>> tasksMap = new HashMap<>();
+    public HashMap<Integer, ArrayList<String>> nameMap = new HashMap<>();
+    private static int helper = 0;
 
     public Timetable() {
 
     }
     public Optional<String> addTask(String name, int day, int duration, boolean priority) {
+        helper += 1;
+        String taskCode = "T" + helper;
+        for (Integer oneDay : tasksMap.keySet()) {
+            if (oneDay.equals(day)) {
+                if (tasksMap.get(oneDay).size() == 5 && duration > 5) {
+                    return Optional.empty();
+                }
+                if (tasksMap.get(oneDay).size() + duration > 5) {
+                    return Optional.empty();
+                }
+                if (nameMap.get(day).contains(name)) {
+                    return Optional.empty();
+                }
+                else {
+                    tasksMap.get(oneDay).add(taskCode);
+                    nameMap.get(oneDay).add(name);
+                    return Optional.of(taskCode);
+                }
+            }
+        }
+        if (duration <= 5 && duration >= 1 && day >= 1) {
+            ArrayList<String> list = new ArrayList<>();
+            ArrayList<String> names = new ArrayList<>();
+            list.add(taskCode);
+            names.add(name);
+            tasksMap.put(day, list);
+            nameMap.put(day, names);
+            return Optional.of(taskCode);
+        }
         return Optional.empty();
     }
 

b458dd54d5780624cf401235f9768c2b562742a5
jevolo <jevolo@ttu.ee>
2022-05-03 09:49:57 +0300

    Delivery

--- EXAM/EXAM0/src/ee/taltech/iti0202/exam/timetable/Task.java
+++ EXAM/EXAM0/src/ee/taltech/iti0202/exam/timetable/Task.java
@@ -1,4 +1,50 @@
 package ee.taltech.iti0202.exam.timetable;
 
 public class Task {
+
+    private String name;
+    private int day;
+    private int duration;
+    private boolean priority;
+    private String taskCode;
+    private boolean isDone = false;
+
+    public Task(String name, int day, int duration, boolean priority) {
+        this.name = name;
+        this.day = day;
+        this.duration = duration;
+        this.priority = priority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public boolean isPriority() {
+        return priority;
+    }
+
+    public String getTaskCode() {
+        return taskCode;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setTaskCode(String taskCode) {
+        this.taskCode = taskCode;
+    }
+
+    public void markAsDone() {
+        isDone = false;
+    }
 }
--- EXAM/EXAM0/src/ee/taltech/iti0202/exam/timetable/Timetable.java
+++ EXAM/EXAM0/src/ee/taltech/iti0202/exam/timetable/Timetable.java
@@ -3,46 +3,65 @@ package ee.taltech.iti0202.exam.timetable;
 import java.util.*;
 
 public class Timetable {
-    public HashMap<Integer, Integer> capacityMap = new HashMap<>();
+    public HashMap<Integer, Integer> dayCapacity = new HashMap<>();
     public HashMap<Integer, ArrayList<String>> nameMap = new HashMap<>();
     public HashMap<String, Boolean> taskDone = new HashMap<>();
+    public HashMap<Integer, ArrayList<Task>> dayMap = new HashMap<>();
+    public ArrayList<Task> taskHolder = new ArrayList<>();
     private static int helper = 0;
 
     public Timetable() {
 
     }
     public Optional<String> addTask(String name, int day, int duration, boolean priority) {
+        Task newTask = new Task(name, day, duration, priority);
         helper += 1;
         String taskCode = "T" + helper;
-        for (Integer oneDay : capacityMap.keySet()) {
+        newTask.setTaskCode(taskCode);
+        for (Integer oneDay : dayMap.keySet()) {
             if (oneDay.equals(day)) {
-                if (capacityMap.get(oneDay) + duration > 5 || duration < 1) {
+                if (dayCapacity.get(oneDay) + duration > 5 || duration < 1) {
                     return Optional.empty();
                 }
                 if (nameMap.get(oneDay).contains(name)) {
                     return Optional.empty();
                 }
                 else {
-                    int  toPlus = capacityMap.get(oneDay);
-                    capacityMap.replace(oneDay, toPlus + duration);
+                    int  toPlus = dayCapacity.get(oneDay);
+                    dayCapacity.replace(oneDay, toPlus + duration);
                     nameMap.get(oneDay).add(name);
-                    taskDone.put(taskCode, false);
+                    dayMap.get(oneDay).add(newTask);
+                    taskHolder.add(newTask);
                     return Optional.of(taskCode);
                 }
             }
         }
         if (duration <= 5 && duration >= 1 && day >= 1) {
             ArrayList<String> names = new ArrayList<>();
+            ArrayList<Task> tasks = new ArrayList<>();
             names.add(name);
-            capacityMap.put(day, duration);
+            tasks.add(newTask);
+            dayCapacity.put(day, duration);
             nameMap.put(day, names);
-            taskDone.put(taskCode, false);
+            dayMap.put(day, tasks);
+            taskHolder.add(newTask);
             return Optional.of(taskCode);
         }
         return Optional.empty();
     }
 
     public boolean markTaskDone(String taskNumber) {
+        for (Task task : taskHolder) {
+            if (task.getTaskCode().equals(taskNumber)) {
+                if (!task.isDone()) {
+                    task.markAsDone();
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
         if (taskDone.containsKey(taskNumber)) {
             if (taskDone.get(taskNumber).equals(false)) {
                 taskDone.replace(taskNumber, true);

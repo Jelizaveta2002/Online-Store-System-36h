e5fd0d2b2a2939e6d857ccb8fa46d592ce11492f
jevolo <jevolo@ttu.ee>
2022-05-03 08:31:16 +0300

    Delivery

--- EXAM/EXAM0/src/ee/taltech/iti0202/exam/timetable/Timetable.java
+++ EXAM/EXAM0/src/ee/taltech/iti0202/exam/timetable/Timetable.java
@@ -3,9 +3,7 @@ package ee.taltech.iti0202.exam.timetable;
 import java.util.*;
 
 public class Timetable {
-    public HashMap<String, Integer> tasks = new HashMap<>();
-    public HashMap<Integer, Integer> days = new HashMap<>();
-    public HashMap<Integer, ArrayList<String>> tasksMap = new HashMap<>();
+    public HashMap<Integer, Integer> capacityMap = new HashMap<>();
     public HashMap<Integer, ArrayList<String>> nameMap = new HashMap<>();
     private static int helper = 0;
 
@@ -15,30 +13,26 @@ public class Timetable {
     public Optional<String> addTask(String name, int day, int duration, boolean priority) {
         helper += 1;
         String taskCode = "T" + helper;
-        for (Integer oneDay : tasksMap.keySet()) {
+        for (Integer oneDay : capacityMap.keySet()) {
             if (oneDay.equals(day)) {
-                if (tasksMap.get(oneDay).size() == 5 || duration > 5) {
-                    return Optional.empty();
-                }
-                if ((tasksMap.get(oneDay).size() + duration) > 5) {
+                if (capacityMap.get(oneDay) + duration > 5) {
                     return Optional.empty();
                 }
                 if (nameMap.get(oneDay).contains(name)) {
                     return Optional.empty();
                 }
                 else {
-                    tasksMap.get(oneDay).add(taskCode);
+                    int  toPlus = capacityMap.get(oneDay);
+                    capacityMap.replace(oneDay, toPlus + duration);
                     nameMap.get(oneDay).add(name);
                     return Optional.of(taskCode);
                 }
             }
         }
         if (duration <= 5 && duration >= 1 && day >= 1) {
-            ArrayList<String> list = new ArrayList<>();
             ArrayList<String> names = new ArrayList<>();
-            list.add(taskCode);
             names.add(name);
-            tasksMap.put(day, list);
+            capacityMap.put(day, duration);
             nameMap.put(day, names);
             return Optional.of(taskCode);
         }
@@ -53,16 +47,16 @@ public class Timetable {
         return null;
     }
 
-//    public static void main(String[] args) {
-//        Timetable timetable = new Timetable();
-//
-//        String task1 = timetable.addTask("wake up1", 1, 1, false).get();
-//        String task2 = timetable.addTask("wake up2", 1, 1, false).get();
-//        String task3 = timetable.addTask("wake up3", 1, 1, false).get();
-//        String task4 = timetable.addTask("wake up4", 1, 1, false).get();
-//        String task5 = timetable.addTask("wake up4", 1, 1, false).get();
-//        //Optional<String> task6 = timetable.addTask("wake up6", 1, 1, false);
-//        //System.out.println(task6); // Optional.empty(), day already full
-//        System.out.println(timetable.nameMap);
-//    }
+    public static void main(String[] args) {
+        Timetable timetable = new Timetable();
+
+        String task1 = timetable.addTask("wake up1", 1, 1, false).get();
+        String task2 = timetable.addTask("wake up2", 1, 1, false).get();
+        String task3 = timetable.addTask("wake up3", 1, 1, false).get();
+        String task4 = timetable.addTask("wake up4", 1, 1, false).get();
+        String task5 = timetable.addTask("wake up4", 1, 1, false).get();
+        Optional<String> task6 = timetable.addTask("wake up6", 1, 1, false);
+        System.out.println(task6); // Optional.empty(), day already full
+        System.out.println(timetable.nameMap);
+    }
 }

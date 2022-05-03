96a692336a8186fba247c3637bfb83b5eba34031
jevolo <jevolo@ttu.ee>
2022-05-03 08:46:31 +0300

    Delivery

--- EXAM/EXAM0/src/ee/taltech/iti0202/exam/timetable/Timetable.java
+++ EXAM/EXAM0/src/ee/taltech/iti0202/exam/timetable/Timetable.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Timetable {
     public HashMap<Integer, Integer> capacityMap = new HashMap<>();
     public HashMap<Integer, ArrayList<String>> nameMap = new HashMap<>();
+    public HashMap<String, Boolean> taskDone = new HashMap<>();
     private static int helper = 0;
 
     public Timetable() {
@@ -25,6 +26,7 @@ public class Timetable {
                     int  toPlus = capacityMap.get(oneDay);
                     capacityMap.replace(oneDay, toPlus + duration);
                     nameMap.get(oneDay).add(name);
+                    taskDone.put(taskCode, priority);
                     return Optional.of(taskCode);
                 }
             }
@@ -34,12 +36,19 @@ public class Timetable {
             names.add(name);
             capacityMap.put(day, duration);
             nameMap.put(day, names);
+            taskDone.put(taskCode, priority);
             return Optional.of(taskCode);
         }
         return Optional.empty();
     }
 
     public boolean markTaskDone(String taskNumber) {
+        if (taskDone.containsKey(taskNumber)) {
+            if (taskDone.get(taskNumber).equals(false)) {
+                taskDone.replace(taskNumber, true);
+                return true;
+            }
+        }
         return false;
     }
 
@@ -54,9 +63,12 @@ public class Timetable {
         String task2 = timetable.addTask("wake up2", 1, 1, false).get();
         String task3 = timetable.addTask("wake up3", 1, 1, false).get();
         String task4 = timetable.addTask("wake up4", 1, 1, false).get();
-        String task5 = timetable.addTask("wake up4", 1, 1, false).get();
+        String task5 = timetable.addTask("wake up5", 1, 1, false).get();
         Optional<String> task6 = timetable.addTask("wake up6", 1, 1, false);
         System.out.println(task6); // Optional.empty(), day already full
-        System.out.println(timetable.nameMap);
+
+        timetable.addTask("swim", 4, 3, true).get();
+// cannot have the same task name on the same day (swim), whatever the other parameters are
+        System.out.println(timetable.addTask("swim", 4, 1, false)); // Optional.empty
     }
 }
